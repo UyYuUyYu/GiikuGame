22eeb33edb555b9a6cff56f24a71cfcbf1c8c58d
{UyYuUyYu} <yuyu06120429@gmail.com>
2023-07-01 03:06:26 +0900

    MolCal

--- Assets/yuuyu/Script/MolCardArea.cs
+++ Assets/yuuyu/Script/MolCardArea.cs
@@ -7,6 +7,14 @@ public class MolCardArea : MonoBehaviour
     [SerializeField] private GameObject[] _MolCard;
     private List<int> _MolCardNumber = new List<int>();
 
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MolCalculation();
+        }
+    }
     //MolCardをカードのエリアに生成する
     public void GenerateMolCard(int n)
     {
@@ -31,5 +39,15 @@ public class MolCardArea : MonoBehaviour
     {
         //ListのmolCardの情報を消去
         _MolCardNumber.RemoveRange(0, _MolCardNumber.Count);
+    }
+
+    public void MolCalculation()
+    {
+        int goukei = 0;
+        for(int i = 0; i < _MolCardNumber.Count; i++)
+        {
+            goukei = goukei + _MolCard[_MolCardNumber[i]].GetComponent<MolcardInfo>().mass;
+        }
+        //print(goukei);
     }
 }
