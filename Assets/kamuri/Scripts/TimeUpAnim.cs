0c8abf53b85c8c833db1520de23c59e06ca15b88
Kamuri96 <mitsu20010909@gmail.com>
2023-07-02 01:43:10 +0900

    transitionScripts

---
+++ Assets/kamuri/Scripts/TimeUpAnim.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+public class TimeUpAnim : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timeupText;
+    float duration = 1f;
+    float scaleMult = 2f;
+
+    void Start()
+    {
+        timeupText.transform.SetAsLastSibling();
+        timeupText.transform.localScale = Vector3.zero;
+        timeupText.color = new Color(timeupText.color.r, timeupText.color.g, timeupText.color.g, 0f);
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(timeupText.DOFade(0f, 0.5f));
+        sequence.Append(timeupText.transform.DOScale(Vector3.zero, 0f));
+        sequence.AppendCallback(() => timeupText.gameObject.SetActive(true));
+        sequence.Append(timeupText.DOFade(1f, duration * 0.4f));
+        sequence.Join(timeupText.transform.DOScale(Vector3.one * scaleMult, duration * 0.7f)
+            .SetEase(Ease.OutBack));
+        sequence.Append(timeupText.transform.DOScale(Vector3.zero, duration * 0.7f)
+           .SetDelay(duration * 0.6f)
+           .SetEase(Ease.InBack));
+        sequence.Join(timeupText.DOFade(0f, duration * 0.4f)
+            .SetDelay(duration * 0.6f));
+        sequence.OnComplete(() => timeupText.gameObject.SetActive(false));
+
+        sequence.Play();
+    }
+}
